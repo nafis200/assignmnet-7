50c110b550a089b4e96f3c3defbe2b75a1a5fb44
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-15 23:46:45 +0600

    second commit

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            
+            <main className="container mx-auto">
+    <div className="lg:bg-[url('https://i.ibb.co/VqcjXd9/depositphotos-42930411-stock-photo-concentrated-male-chef-garnishing-food.jpg')] bg-[url('https://i.ibb.co/ySRV2wm/depositphotos-42930411-stock-photo-concentrated-male-chef-garnishing-food-2.jpg')] bg-no-repeat h-96  w-6/7 lg:ml-20  lg:w-full mt-6 ml-2 mr-2">
+    </div>
+  </main>
+
+
+        </div>
+    );
+};
+
+export default Banner;
