0f5c39c0d503bc3851cd4b4310b1451633e16417
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-17 10:53:09 +0600

    thirtten commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import Banner from "./components/banner/Banner";
 import Navbar from "./components/navbar/Navbar";
 import Cards from "./components/card/Cards";
+import Tables from "./components/table/Tables";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
-      <div>
+      <div className="flex">
         <Cards></Cards>
+        <Tables></Tables>
       </div>
     </>
   );
---
+++ src/components/table/Tables.jsx
@@ -0,0 +1,70 @@
+const Tables = () => {
+  return (
+    <div className="w-1/3 lg:mt-10 mt-12 ml-3 lg:ml-0">
+      <h1 className="font-bold text-center">Want to cook</h1>
+      <hr className="mt-4 w-[400px]" />
+      <section className="">
+        <div class="overflow-x-auto lg:mr-20">
+          <table class="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>
+                  Quality <br /> Specialist
+                </td>
+                <td>Blue</td>
+                <button className="btn mt-2 bg-green-400 ">Preparing</button>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+      <section className="mt-5">
+        <h1 className="font-bold text-center">Current cooking</h1>
+        <hr className="mt-5 w-[400px]" />
+        <div class="overflow-x-auto lg:mr-20">
+          <table class="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>
+                  Quality <br /> Specialist
+                </td>
+                <td>Blue</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="flex gap-2 lg:gap-12 mt-3 lg:ml-32">
+            <p className="font-bold">
+              total time = <br /> <span>45 minutes</span>
+            </p>
+            <p className="font-bold">
+              Total calories = <br /> <span>1000 calories</span>{" "}
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Tables;
+
